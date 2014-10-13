73da6ff6d0cd345914c60a3e8ecb2d72fb63eead
1999mani <1999mani@gmail.com>
2014-10-13 02:42:38 +0200

    Added untested packet stuff! Testing it tomorow. Good night ;)

--- src/me/mani/panemgames/PlayerManager.java
+++ src/me/mani/panemgames/PlayerManager.java
@@ -3,7 +3,15 @@ package me.mani.panemgames;
 import java.util.Arrays;
 import java.util.List;
 
+import net.minecraft.server.v1_7_R4.ChatSerializer;
 import net.minecraft.server.v1_7_R4.EntityPlayer;
+import net.minecraft.server.v1_7_R4.EnumClientCommand;
+import net.minecraft.server.v1_7_R4.EnumGamemode;
+import net.minecraft.server.v1_7_R4.IChatBaseComponent;
+import net.minecraft.server.v1_7_R4.PacketPlayInClientCommand;
+import net.minecraft.server.v1_7_R4.PacketPlayOutChat;
+import net.minecraft.server.v1_7_R4.PacketPlayOutGameStateChange;
+import net.minecraft.server.v1_7_R4.PacketSplitter;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -12,6 +20,7 @@ import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.scoreboard.Scoreboard;
+import org.spigotmc.ProtocolInjector.PacketPlayResourcePackStatus;
 
 @SuppressWarnings("deprecation")
 public class PlayerManager {
@@ -30,6 +39,21 @@ public class PlayerManager {
 		for (Player p : player) {
 			p.sendMessage(message);
 		}
+	}
+	
+	public static void send(String message, ChatPosition pos, Player p) {
+		IChatBaseComponent text = ChatSerializer.a("{text:\"" + message + "\"}");
+		PacketPlayOutChat packet = new PacketPlayOutChat(text, pos.getID());
+		if (pos.equals(ChatPosition.ABOVE_ACTION_BAR) &&
+				((CraftPlayer) p).getHandle().playerConnection.networkManager.getVersion() < 47)
+			return;
+		((CraftPlayer) p).getHandle().playerConnection.sendPacket(packet);
+	}
+	
+	public static void setSpectatorMode(Player p) {
+		if (((CraftPlayer) p).getHandle().playerConnection.networkManager.getVersion() < 47)
+			return;
+		((CraftPlayer) p).getHandle().playerConnection.sendPacket(new PacketPlayOutGameStateChange(3, 3f));
 	}
 
 	public static void playAll(Sound sound) {
@@ -81,6 +105,22 @@ public class PlayerManager {
 
 	public static List<Player> getAll() {
 		return Arrays.asList(Bukkit.getOnlinePlayers());
+	}
+	
+	public static enum ChatPosition {
+		CHAT_BOX (0),
+		CHAT_BOX_SYSTEM (1),
+		ABOVE_ACTION_BAR (2);
+		
+		private ChatPosition(int i) {
+			this.i = i;
+		}
+		
+		private int i;
+		
+		public int getID() {
+			return this.i;
+		}
 	}
 
 }
